86e0e36ac818a87f4cee148c7dff4027880b2f31
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-04-23 14:43:08 +0600

    added vercel

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ async function run() {
     await client.connect();
 
     const coffeeCollection = client.db("coffeeDB").collection("coffee");
+    const userCollection = client.db("coffeeDB").collection("user");
 
     // get operation
     app.get("/coffee", async (req, res) => {
@@ -81,6 +82,39 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+    // user related apis
+    app.get('/user', async (req, res)=> {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/user', async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+    })
+
+    app.patch("/user", async (req, res)=> {
+      const user = req.body;
+      const filter = { email: user.email};
+      const updatedDoc = {
+        $set : {
+          lastLoggedAt : user.lastLoggedAt,
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    app.delete('/user/:id', async (req, res)=> {
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     })
 
